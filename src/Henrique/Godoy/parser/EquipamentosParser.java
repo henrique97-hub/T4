76cbff650ae5c49a5dbe0ed4650ad1e7f15d85be
henrique97-hub <61150198+henrique97-hub@users.noreply.github.com>
2020-12-03 16:19:15 -0300

    Parsers concluidos

--- src/Henrique/Godoy/parser/EquipamentosParser.java
+++ src/Henrique/Godoy/parser/EquipamentosParser.java
@@ -1,4 +1,30 @@
 package Henrique.Godoy.parser;
 
+import Henrique.Godoy.model.Equipamentos;
+import org.json.JSONObject;
+
+//(s) nome, (s) tipo, (i) poderDeDefesa, (i) defesaNecessaria, (s)beneficio
+
 public class EquipamentosParser {
+    public static JSONObject toJson(Equipamentos equipamentos){
+        JSONObject json = new JSONObject();
+        json.put("nome",equipamentos.getNome());
+        json.put("tipo",equipamentos.getTipo());
+        json.put("poderDeDefesa",equipamentos.getPoderDeDefesa());
+        json.put("defesaNecessaria",equipamentos.getDefesaNecessaria());
+        json.put("beneficio",equipamentos.getBeneficio());
+        return json;
+
+    }
+    public static Equipamentos fromJson(JSONObject json){
+        Equipamentos equipamentos = new Equipamentos(
+                json.getString("nome"),
+                json.getString("tipo"),
+                json.getInt("poderDeDefesa"),
+                json.getInt("defesaNecessaria"),
+                json.getString("beneficio")
+
+        );
+        return equipamentos;
+    }
 }
--- src/Henrique/Godoy/parser/ItensParser.java
+++ src/Henrique/Godoy/parser/ItensParser.java
@@ -1,4 +1,33 @@
 package Henrique.Godoy.parser;
 
+import Henrique.Godoy.model.Itens;
+import org.json.JSONObject;
+
+// (s)nome,(s)tipo,(i)poderDeAtaque, (i)velocidadeArma, (i)ataqueNecessario, (d)critico e (s)beneficio
+
 public class ItensParser {
+    public static JSONObject toJson(Itens itens){
+        JSONObject json = new JSONObject();
+        json.put("nome",itens.getNome());
+        json.put("tipo",itens.getTipo());
+        json.put("poderDeAtaque",itens.getPoderDeAtaque());
+        json.put("velocidadedeArma",itens.getVelocidadeArma());
+        json.put("ataqueNecessario",itens.getAtaqueNecessario());
+        json.put("critico",itens.getCritico());
+        json.put("beneficio",itens.getBeneficio());
+        return json;
+
+    }
+    public static Itens fromJson(JSONObject json){
+        Itens itens = new Itens(
+                json.getString("nome"),
+                json.getString("tipo"),
+                json.getInt("poderDeAtaque"),
+                json.getInt("velocidadedeArma"),
+                json.getInt("ataqueNecessario"),
+                json.getDouble("critico"),
+                json.getString("beneficio")
+        );
+        return itens;
+    }
 }
